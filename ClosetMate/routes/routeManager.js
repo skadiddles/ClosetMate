2868652981b2bb711060cfccdeda2ab7451784e9
Cegeurun <cegeurunws@gmail.com>
2025-09-03 01:36:08 +0800

    Added capacitor to the project Capacitor will be used to convert the webview into a native mobile app.

--- ClosetMate/routes/routeManager.js
+++ ClosetMate/routes/routeManager.js
@@ -2,6 +2,7 @@ import express from 'express';
 import homeRouter from './homeRoute.js';
 import loginRouter from './loginRoute.js';
 
+
 const routeManager = express();
 
 routeManager.use(homeRouter);
